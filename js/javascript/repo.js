3acd8776b94436fd1d0400cd9c3fc7fa64b9ead0
TracksJosh <arcticwolf1526@gmail.com>
2022-12-06 15:17:14 -0600

    something something
    lol

--- js/javascript/repo.js
+++ js/javascript/repo.js
@@ -1,1 +1,15 @@
-const obj = fetch("https://tracksjosh.github.io/repo_of_qb/js/json/tossups.json").then(res => res.json()).then(data=>console.log(data));
+async function getData()
+{
+    const obj = await fetch("https://tracksjosh.github.io/repo_of_qb/js/json/tossups.json");
+	const data = await obj.json();
+	let text = "";
+	for(var i = 1; i <= data.tossups.length; i++)
+	{
+	    text += `<div class="tossup"><p>`+data.tossups[i-1].name+`</p></div>`;	
+	}
+    
+    document.getElementById("query").innerHTML = text;
+}
+
+
+getData();
